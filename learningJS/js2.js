16b8252f909d454de296eed83c391234120b6afd
Rishabh Mittal <122997682+therishabhmittal-05@users.noreply.github.com>
2023-08-27 19:10:19 +0000

    js2.js

---
+++ learningJS/js2.js
@@ -0,0 +1,10 @@
+"use strict";
+const accountID = 22104089
+
+let accountName = 'Rishabh Mittal'
+let accountSubject = 'Electronics and Communication'
+//console.log([accountID, accountName, accountSubject]);
+//console.table([accountID, accountName, accountSubject])
+let x = 4, y = 3
+console.log(x , y++)
+console.log(x , y++)
