997380ff5dc66cfbb209853047e0980c80f68b31
Rishabh Mittal <122997682+therishabhmittal-05@users.noreply.github.com>
2023-08-31 18:07:11 +0000

    after a long time

---
+++ learningJS/js3.js
@@ -0,0 +1,18 @@
+let a = 4;
+let b = a;
+a = 5;
+console.log (a)
+console.log (b)
+console.log (`value of a is ${a} and b is ${b}`);
+const myString = new String('hello World good to be here again')
+let string = '   hello   '
+console.log(myString)
+console.log(typeof(myString))
+console.log(myString.toUpperCase())
+console.log(myString)
+console.log(myString.charAt(2))
+console.log(myString.slice(0,4))
+console.log(string)
+console.log(string.trimEnd())
+console.log(myString.split('o'))
+
---
+++ learningJS/js4.js
@@ -0,0 +1,8 @@
+const score = 400;
+const balance = new Number (500000000)
+
+console.log(score)
+console.log(balance)
+console.log(balance.toLocaleString('en-IN'))
+console.log(balance.toString().split('')[2])
+
