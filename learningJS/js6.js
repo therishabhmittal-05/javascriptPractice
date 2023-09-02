9775d91b6430da0ebb1af83ebfef77ad46fbcc0d
Rishabh Mittal <122997682+therishabhmittal-05@users.noreply.github.com>
2023-09-02 18:08:52 +0000

    js6 and js7

---
+++ learningJS/js6.js
@@ -0,0 +1,12 @@
+// console.log(Math)
+// console.log(Math.abs(-10000000))
+// console.log(Math.pow(2,5))
+// console.log(Math.PI.toFixed(3))
+// console.log(Math.round(4.999))
+// console.log(Math.floor(4.9999))
+// console.log(Math.min(3,4,5,6,12,33))
+// console.log(Math.max(3,4,567,4444))
+// console.log(Math.floor((Math.random()*(6-1+1))+1)) 
+
+
+
