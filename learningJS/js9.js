8305916cb0cb84cedddd766685b7daf468505013
Rishabh Mittal <122997682+therishabhmittal-05@users.noreply.github.com>
2023-09-03 19:48:24 +0000

    js9new

--- learningJS/js9.js
+++ learningJS/js9.js
@@ -1,12 +1,23 @@
-const myUser = {
-    name : 'Rishabh Mittal',
-    age : 19,
-    location : 'Jalandhar',
-    lastLoggedIn : ["monday", "tuesday"]
+// const myUser = {
+//     name : 'Rishabh Mittal',
+//     age : 19,
+//     location : 'Jalandhar',
+//     lastLoggedIn : ["monday", "tuesday"]
+// }
+// // console.log(myUser)
+// // console.log(myUser.name)
+// myUser.email = "rishabh@apple.ai"
+// myUser.Welcome = `Hello, ${myUser.age}`
+// // console.log(myUser['Welcome'])
+// myUser.greetings = ()=>{
+//     console.log(`hello, ${myUser.name}`)
+// }
+// console.log(myUser.greetings())
+const myObj1 = {
+    name : "rishabh"
 }
-// console.log(myUser)
-// console.log(myUser.name)
-myUser.email = "rishabh@apple.ai"
-myUser.Welcome = `Hello, ${myUser.age}`
-console.log(myUser['Welcome'])
-
+const myObj2 = {
+    class : "ECE"
+}
+const myObj3 = Object.assign({},myObj1,myObj2)
+console.log(myObj3===myObj1)
