52633e58dc6bfb45dbddf5884e073e85dba57f3a
Rishabh Mittal <122997682+therishabhmittal-05@users.noreply.github.com>
2023-09-03 11:24:12 +0000

    js8

---
+++ learningJS/js8.js
@@ -0,0 +1,31 @@
+let myArr = [1,2,3,4,["Rishabh Mittal", 22104089]]
+myArr.push("ECE","hello World")
+// console.log(myArr);
+// console.log(myArr.pop());
+// let a = ["Rishabh Mittal", 22104089]
+// myArr.push(a)
+// console.log(myArr[4][0]);
+// console.log(myArr);
+
+// console.log(myArr.includes('hello World'));
+// console.log(myArr.includes(a));
+// console.log(myArr.splice(3)); 
+// console.log(myArr);
+let myArr2 = ["new Array added",]
+let myArr3 = ['practicing  concat and ... method']
+// console.log(myArr.concat(myArr2).concat(myArr3));
+// let newArr = myArr.concat(myArr2).concat(myArr3)
+// console.log(newArr[8]);
+// newArr = [...myArr,...myArr2,...myArr3,...newArr]
+// console.log(newArr)
+// myArr.push(myArr2,myArr3)
+// console.log(myArr.flat(1))
+// console.log(myArr.flat(Infinity))
+myArr.push(myArr,myArr)
+// console.log(myArr)
+let of = Array.from('myArr')
+console.log(of)
+let newArr  = Array.of(myArr,myArr2,myArr3)
+console.log(newArr);
+
+
