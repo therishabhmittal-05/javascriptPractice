39ef0f4fb630c3116d632c75d87d4360326fd771
Rishabh Mittal <122997682+therishabhmittal-05@users.noreply.github.com>
2023-09-03 19:04:45 +0000

    js9

---
+++ learningJS/js9.js
@@ -0,0 +1,12 @@
+const myUser = {
+    name : 'Rishabh Mittal',
+    age : 19,
+    location : 'Jalandhar',
+    lastLoggedIn : ["monday", "tuesday"]
+}
+// console.log(myUser)
+// console.log(myUser.name)
+myUser.email = "rishabh@apple.ai"
+myUser.Welcome = `Hello, ${myUser.age}`
+console.log(myUser['Welcome'])
+
