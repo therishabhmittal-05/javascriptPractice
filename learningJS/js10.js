7a7ee68b8b409f4ca5835bd33e3d4fd39c837dc0
Rishabh Mittal <122997682+therishabhmittal-05@users.noreply.github.com>
2023-09-06 19:53:21 +0000

    js10

---
+++ learningJS/js10.js
@@ -0,0 +1,29 @@
+// const user1 = {
+//     a : 'Rishabh Mittal',
+//     1 : 'ECE',
+//     3 : 221040479,
+//      4 : "NITJ"
+// }
+// const user2 = {
+//     name : 'Rishabh Mittal',
+//     class : 'ECEWW',
+//     RollNo : 221040479,
+//     college : "NITJ"
+// }
+// const userDatabase = {user1, user2, user3}
+// console.log(userDatabase);
+// console.log(user1[])
+// console.log( (Object.keys(user1).length));
+// console.log(Object.values(user2))
+const user3 = {
+    name : 'Rishabh Mittal',
+    class : 'ECE',
+    RollNo : 221040479,
+    college : "NITJ"
+}
+const {RollNo : a2} = user3;
+console.log(a2)
+
+
+
+
