a9a8b115af952af57602b90661b1b6930f41b4a7
Rishabh Mittal <122997682+therishabhmittal-05@users.noreply.github.com>
2023-09-09 19:31:06 +0000

    js13

---
+++ learningJS/js13.js
@@ -0,0 +1,39 @@
+// let a = 10
+// var c = 100
+// if (true){
+//     var c= 122
+//     let a = 111
+// console.log(a)
+
+// }
+// console.log(a)
+// console.log(c)
+
+
+
+// console.log(addone(2))
+
+// function addone (num){
+//     return num +1 
+// }
+// console.log(addTwo(2))
+
+// const addTwo = function(num){
+//     return num + 2
+// }
+
+
+const user = {
+    name : "Rishabh",
+    class : 'ECE',
+    writeMessage : function (){
+        console.log(`${this.name}, welcome to ${this.class}`)
+        console.log(this)
+    }
+}
+user.writeMessage()
+user.name = 'Sammy'
+user.writeMessage()
+
+console.log(this)
+
