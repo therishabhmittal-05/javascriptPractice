91b00af2d3512eda84499f900339f79da1f60f95
Rishabh Mittal <122997682+therishabhmittal-05@users.noreply.github.com>
2023-09-26 04:19:41 +0000

    js18

---
+++ learningJS/js18.js
@@ -0,0 +1,33 @@
+const myString = 'Rishabh Mittal'
+const myStr = new   String('Rishabh Mittal')
+// console.log(myString.indexOf('Mittal'))
+// console.log(myString.length)
+// console.log(myString.substring(1,4))
+// console.log(typeof myStr)
+// console.log(typeof myString)
+// console.log(`my name is ${myStr} and people also call me ${myString}`)
+// console.log('cat'.charAt(2))
+// console.log('cat'[1])
+// console.log(myStr.charCodeAt(0))
+// const myObject = {}
+// Object.defineProperty(myObject, property1 , {name : Rishabh}) 
+// console.log(myObject.property1)
+// const a = "a";
+// const b = "a";
+// if (a < b) {
+//   // true
+//   console.log(`${a} is less than ${b}`);
+// } else if (a > b) {
+//   console.log(`${a} is greater than ${b}`);
+// } else {
+//   console.log(`${a} and ${b} are equal.`);
+// }
+// function myStringCompare (str1, str2){
+//     if (str1.toUpperCase()==str2.toUpperCase())
+//     return true
+// else return'error'
+// }
+// console.log(myStringCompare(myStr,myString))
+// console.log(myStr.toUpperCase())
+// console.log(myStr.endsWith())
+console.log('qwertyuiop'.toLocaleUpperCase('tr'))
