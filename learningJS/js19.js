a1e1b7c5d89c868778e46ea1e511799c14d75abb
Rishabh Mittal <122997682+therishabhmittal-05@users.noreply.github.com>
2023-09-28 06:58:23 +0000

    js19 string and numbers

---
+++ learningJS/js19.js
@@ -0,0 +1,26 @@
+const myObject = {
+    name: 'Rishabh Mittal',
+    rollNo : 22104089
+}
+// console.log(typeof myObject);
+// console.log(tostring(myObject))
+const myStr1 = '   Rishabh   Mittal '
+const myStr2 = '    Mittal   '
+// console.log(myStr1.concat(myStr1,myStr2))
+// console.log(myObject.name.includes('ishabh Mi'))
+// console.log(myStr1.replace('R','r'))
+// // console.log(myStr1.splice(0,3))
+// console.log(typeof myObject.rollNo)
+// console.log(typeof myObject.rollNo.toString())
+console.log(myStr1.blink())
+console.log(myStr1.trim())
+myStr2.trim()
+myObject.rollNo.toString()
+console.log(typeof myObject.rollNo);
+const myNumber = new Number(22104058)
+console.log(typeof myNumber)
+console.log(Number.MAX_VALUE + Number.MAX_VALUE)
+console.log(Number.EPSILON)
+console.log(Number.MIN_VALUE)
+console.log(Number.NaN)
+console.log(Number.isFinite(Number.MAX_VALUE))
