f09510bb189734cd0598f4335b5067f459f14bcb
Rishabh Mittal <122997682+therishabhmittal-05@users.noreply.github.com>
2023-09-29 11:39:46 +0000

    js22

---
+++ learningJS/js22.js
@@ -0,0 +1,29 @@
+// for (let i = 0; i <= 5; i++) {
+//     console.log(`outer loop ${i}`); 
+//     for (let j = 0; j <= 5; j++) {
+//         console.log(`inner loop J: ${j} and inner loop I: ${i}`);     
+//     }  
+// }
+
+// Multipication Tables
+// for (let i = 21; i <= 30; i++) {
+//     console.log(`table of ${i}`);
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = `+ i*j)    
+//     } 
+// }
+
+
+
+// BREAK AND CONTINUE
+
+
+for (let i = 0; i < 10; i++) {
+    
+    if (i==2){
+     
+    }
+    console.log('value of i is '+ i)
+    break
+    
+}
