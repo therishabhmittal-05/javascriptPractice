30feaa954ec1d92a547d1a91379c5e0229aeb94a
therishabhmittal05 <rishabhmittal928@gmail.com>
2024-03-27 17:07:00 +0530

    jsRevision

---
+++ jsRevision.js
@@ -0,0 +1,27 @@
+// let count = 0;
+// const counter = setInterval(function(){
+//     count++;
+//     console.log(count)
+// },1000)
+// setTimeout(function(){
+//     clearInterval(counter)
+// },11*1000)
+
+// setInterval(function(){
+//     const myDate = new Date();
+//     const hours = myDate.getHours()
+//     const minutes = myDate.getMinutes()
+//     const seconds = myDate.getSeconds()
+//     console.log(`Time=> ${hours}:${minutes}:${seconds} `)
+
+// },1000)
+////how to create custom callback other than setTimeout, setInterval, clearInterval
+// function sayHello(){
+//     console.log("SayHello 1")
+//     function sayhello2(){
+//         console.log("SayHello 2")
+//     }
+//     sayhello2();
+
+// }
+// sayHello()
